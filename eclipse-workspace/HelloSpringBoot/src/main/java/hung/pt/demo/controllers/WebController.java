dfe239f0c493e82afff67ba69b5f787ec9930a9a
The Hung <thehung622k2@gmail.com>
2024-03-08 13:09:10 +0700

    up HelloSpringBoot

---
+++ eclipse-workspace/HelloSpringBoot/src/main/java/hung/pt/demo/controllers/WebController.java
@@ -0,0 +1,14 @@
+package hung.pt.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/")
+	public String index()
+	{
+		return "index";
+	}
+}
