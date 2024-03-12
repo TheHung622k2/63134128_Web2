5a338c37b627c8526d6156378b1639f2203bb76d
The Hung <thehung622k2@gmail.com>
2024-03-12 10:31:08 +0700

    Up bài tập tuần 8

--- eclipse-workspace/HelloSpringBoot/src/main/java/hung/pt/demo/controllers/WebController.java
+++ eclipse-workspace/HelloSpringBoot/src/main/java/hung/pt/demo/controllers/WebController.java
@@ -3,12 +3,15 @@ package hung.pt.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// Đánh dấu đây là một Controller
+// Nơi tiếp nhận các request từ phía người dùng
 @Controller
 public class WebController {
 
-	@GetMapping("/")
+	// Đón nhận request GET
+	@GetMapping("/") // Nếu người dùng request tới địa chỉ "/"
 	public String index()
 	{
-		return "index";
+		return "index"; // Trả về file index.html
 	}
 }
