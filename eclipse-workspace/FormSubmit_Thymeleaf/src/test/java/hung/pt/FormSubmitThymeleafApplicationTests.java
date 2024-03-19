4ab8157e23d4cc00224f927b9150de5c3ed4696a
The Hung <thehung622k2@gmail.com>
2024-03-19 09:43:47 +0700

    up FormSubmit_Thymeleaf

---
+++ eclipse-workspace/FormSubmit_Thymeleaf/src/test/java/hung/pt/FormSubmitThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package hung.pt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormSubmitThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
