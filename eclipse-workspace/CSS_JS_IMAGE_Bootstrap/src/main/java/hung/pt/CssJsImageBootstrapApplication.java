ecede13f14e06faa2a9af2dc64cde8b5d45f9de8
The Hung <thehung622k2@gmail.com>
2024-03-25 10:20:19 +0700

    up CSS_JS_IMAGE_Bootstrap

---
+++ eclipse-workspace/CSS_JS_IMAGE_Bootstrap/src/main/java/hung/pt/CssJsImageBootstrapApplication.java
@@ -0,0 +1,13 @@
+package hung.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssJsImageBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssJsImageBootstrapApplication.class, args);
+	}
+
+}
