b60c7dd17f6fa8d2d0c67de709830086f30aa3f4
The Hung <thehung622k2@gmail.com>
2024-03-26 14:55:53 +0700

    tao du an

---
+++ eclipse-workspace/phamthehung_QLSinhVien/src/main/java/thiGK/ntu63134128/PhamthehungQlSinhVienApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63134128;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamthehungQlSinhVienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamthehungQlSinhVienApplication.class, args);
+	}
+
+}
---
+++ eclipse-workspace/phamthehung_QLSinhVien/src/test/java/thiGK/ntu63134128/PhamthehungQlSinhVienApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu63134128;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamthehungQlSinhVienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
