e110c5b4566d9b5b90988d03bd679ef1645f20c0
The Hung <thehung622k2@gmail.com>
2024-03-26 16:12:52 +0700

    up

--- eclipse-workspace/phamthehung_QLSinhVien/src/main/java/thiGK/ntu63134128/controller/StudentController.java
+++ eclipse-workspace/phamthehung_QLSinhVien/src/main/java/thiGK/ntu63134128/controller/StudentController.java
@@ -3,8 +3,10 @@ package thiGK.ntu63134128.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
+import thiGK.ntu63134128.models.DTOStudent;
 import thiGK.ntu63134128.service.StudentService;
 
 @Controller
@@ -13,9 +15,29 @@ public class StudentController {
     @Autowired
     private StudentService studentService;
 
+    // hien thi danh sach
     @GetMapping("/students")
     public String showStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+    
+    // Hien thi form cho phep them moi sinh vien
+    @GetMapping("/add-student-form")
+    public String showAddStudentForm(Model model) {
+        model.addAttribute("student", new DTOStudent());
+        return "add-student-form";
+    }
+    
+    // Tim kiem thong qua ma
+    @GetMapping("/search")
+    public String searchStudentById(@RequestParam("id") String id, Model model) {
+    	DTOStudent student = studentService.getStudentById(id);
+        if (student != null) {
+            model.addAttribute("student", student);
+            return "student-details";
+        } else {
+            return "student-not-found";
+        }
     }
 }
