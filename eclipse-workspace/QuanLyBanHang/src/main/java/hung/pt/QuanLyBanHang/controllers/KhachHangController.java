32a9498b9679451339600ab01fcd80c83a0be824
The Hung <thehung622k2@gmail.com>
2024-04-16 15:04:54 +0700

    Tạo dự án thi cuối kỳ

--- eclipse-workspace/QuanLyBanHang/src/main/java/hung/pt/QuanLyBanHang/controllers/KhachHangController.java
+++ eclipse-workspace/QuanLyBanHang/src/main/java/hung/pt/QuanLyBanHang/controllers/KhachHangController.java
@@ -3,18 +3,21 @@ package hung.pt.QuanLyBanHang.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import hung.pt.QuanLyBanHang.models.KhachHang;
 import hung.pt.QuanLyBanHang.services.KhachHangService;
 
-@RestController
+@Controller
+@RequestMapping("/customer")
 public class KhachHangController {
-	@Autowired KhachHangService khachHangService;
-	@GetMapping("/dsKhachHang")
-	public List<KhachHang> getAllKhachHang()
-	{
-		return khachHangService.getAllKhachHang();
+	@Autowired KhachHangService customerService;
+	@GetMapping("/all")
+	public String getAll(Model model) {
+		List<KhachHang> dsKH = customerService.getAllKhachHang();
+		model.addAttribute("dsKhachHang", dsKH);
+		return "index";
 	}
 }
