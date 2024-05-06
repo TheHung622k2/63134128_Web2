a3a12f108f32972f3b2a55fe1ecccbdeb7bea91f
The Hung Pham <131739098+TheHung622k2@users.noreply.github.com>
2024-05-06 23:19:54 +0700

    Cập nhật code Đăng Nhập

--- eclipse-workspace/DuAnCuoiKy/src/main/java/hung/pt/QuanLyBanHang/Controllers/NguoiDungController.java
+++ eclipse-workspace/DuAnCuoiKy/src/main/java/hung/pt/QuanLyBanHang/Controllers/NguoiDungController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hung.pt.QuanLyBanHang.Models.NguoiDung;
 import hung.pt.QuanLyBanHang.Services.NguoiDungService;
@@ -23,5 +25,26 @@ public class NguoiDungController {
 		List<NguoiDung> dsNguoiDung = nguoiDungService.getAllNguoiDungs();
 		model.addAttribute("dsNguoiDung", dsNguoiDung);
 		return "nguoidung/nguoidung";
+	}
+	
+	@PostMapping("/login")
+	public String login(@RequestParam("username") String username,
+	                    @RequestParam("password") String password, Model model) {
+	    List<NguoiDung> nguoiDungs = nguoiDungService.getNguoiDungsByTenDangNhap(username);
+	    boolean isAuthenticated = false;
+
+	    for (NguoiDung nguoiDung : nguoiDungs) {
+	        if (nguoiDung.getMatKhau().equals(password)) {
+	            isAuthenticated = true;
+	            break;
+	        }
+	    }
+
+	    if (isAuthenticated) {
+	        return "redirect:/admin.html"; // chuyển hướng tới admin.html
+	    } else {
+	        model.addAttribute("error", "Tên đăng nhập hoặc mật khẩu không đúng!");
+	        return "sign_in"; // trả về trang đăng nhập với thông báo lỗi
+	    }
 	}
 }
